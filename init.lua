38819d8f5b3d96169d51853b1e98ad03182820f5
zedddie16 <zedddiezxc@gmail.com>
2025-04-24 01:04:56 +0200

    removed redundant ui array

--- init.lua
+++ init.lua
@@ -1104,27 +1104,27 @@ require('lazy').setup {
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
-  {
-    ui = {
-      -- If you are using a Nerd Font: set icons to an empty table which will use the
-      -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-      icons = vim.g.have_nerd_font and {} or {
-        cmd = '⌘',
-        config = '🛠',
-        event = '📅',
-        ft = '📂',
-        init = '⚙',
-        keys = '🗝',
-        plugin = '🔌',
-        runtime = '💻',
-        require = '🌙',
-        source = '📄',
-        start = '🚀',
-        task = '📌',
-        lazy = '💤 ',
-      },
-    },
-  },
+  -- {
+  --   ui = {
+  --     -- If you are using a Nerd Font: set icons to an empty table which will use the
+  --     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+  --     icons = vim.g.have_nerd_font and {} or {
+  --       cmd = '⌘',
+  --       config = '🛠',
+  --       event = '📅',
+  --       ft = '📂',
+  --       init = '⚙',
+  --       keys = '🗝',
+  --       plugin = '🔌',
+  --       runtime = '💻',
+  --       require = '🌙',
+  --       source = '📄',
+  --       start = '🚀',
+  --       task = '📌',
+  --       lazy = '💤 ',
+  --     },
+  --   },
+  -- },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
