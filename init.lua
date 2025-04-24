02501dba131d822a9767a0060efacfa1476e48a4
zedddie16 <zedddiezxc@gmail.com>
2025-04-24 20:01:53 +0200

    refactor: moved themes to different lua file from init.lua

--- init.lua
+++ init.lua
@@ -1055,6 +1055,8 @@ require('lazy').setup {
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'my_plugins.harpoon2',
 
+  require 'my_plugins.themes',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
