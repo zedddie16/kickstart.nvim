26c8fb221ce38badf7c297b9753c1d6d85a8df11
zedddie16 <zedddiezxc@gmail.com>
2025-05-02 10:00:31 +0200

    added sql lsp

--- init.lua
+++ init.lua
@@ -621,6 +621,7 @@ require('lazy').setup {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
+        sqls = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
