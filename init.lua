62f6cb75126cf0553c666e019227d8b7dbcf9bfd
zedddie16 <zedddiezxc@gmail.com>
2025-05-15 16:02:55 +0200

    transparency via config instead of term

--- init.lua
+++ init.lua
@@ -987,7 +987,7 @@ require('lazy').setup {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'kanagawa'
+      vim.cmd.colorscheme 'rose-pine'
     end,
   },
 
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,6 @@ return {
   require 'my_plugins.themes',
   require 'my_plugins.linters',
   require 'my_plugins.starters',
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' }),
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' }),
 }
