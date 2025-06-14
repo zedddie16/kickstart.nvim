8b82c02f02f1f69fa22782751055d6f3476af97a
zedddie16 <zedddiezxc@gmail.com>
2025-06-14 15:09:31 +0200

    feat: kanagawa theme settings

--- lua/my_plugins/themes.lua
+++ lua/my_plugins/themes.lua
@@ -12,7 +12,40 @@ return {
   { 'olimorris/onedarkpro.nvim' },
   { 'maxmx03/solarized.nvim' },
   { 'gbprod/nord.nvim' },
-  { 'rebelot/kanagawa.nvim' },
+  {
+    'rebelot/kanagawa.nvim',
+    name = 'kanagawa',
+    config = function()
+      -- Default options:
+      require('kanagawa').setup {
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = false },
+        functionStyle = {},
+        keywordStyle = { italic = false },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = { -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = 'wave', -- Load "wave" theme
+        background = { -- map the value of 'background' option to a theme
+          dark = 'dragon', -- try "dragon" !
+          light = 'lotus',
+        },
+      }
+
+      -- setup must be called before loading
+      vim.cmd 'colorscheme kanagawa'
+    end,
+  },
   { 'mhartington/oceanic-next' },
   { 'sainnhe/everforest' },
   { 'zedddie16/pastelpink' },
